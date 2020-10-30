65ecf92b50f11e470e58792c7082096b8e877d88
Abisai Ramos <abisaigomez6@gmail.com>
2020-10-30 11:43:53 -0600

    responding an object instead simple array for responde equal data

--- persistence/filesystem.js
+++ persistence/filesystem.js
@@ -4,5 +4,15 @@ const databasePath = fs.readFileSync(`${__dirname}/db.json`, 'utf8')
 const myApps = JSON.parse(databasePath)
 const installedApps = fs.readdirSync('/usr/bin', 'utf8')
 
+function parseInstalledApps() {
+	return installedApps.map(app => {
+		return {
+			name: app,
+			keywords: app,
+			cmd: app
+		}
+	})
+}
+
 exports.myApps = myApps
-exports.installedApps = installedApps
+exports.installedApps = parseInstalledApps()
