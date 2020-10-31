7383844313a358f7bb8a76587271f5effa3f3c63
Abisai Ramos <abisaigomez6@gmail.com>
2020-10-30 21:01:33 -0600

    bug found while I was adding test, now I fixed it

--- src/application.js
+++ src/application.js
@@ -7,10 +7,12 @@ function toLowerCase(string) {
 }
 
 function toUpperCaseFirstLetter(string) {
-	if (string.length !== 1)
-		return string
-	return string.charAt(0).toUpperCase()
-}
+	if (string.length == 1)
+		return string.toUpperCase()
+
+	const firstLetter = string.charAt(0).toUpperCase()
+	const remainingLetters = string.slice(1)
+	return firstLetter.concat(remainingLetters)}
 
 exports.isEmptyArray = isEmptyArray
 exports.toLowerCase = toLowerCase
